59989dfec4500e17c4d34bad4d59b3f8487490e7
rodrigo <javierluis15975@hotmail.com>
2025-01-12 01:04:33 -0500

    update task.module.ts

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TaskService } from './task.service'
 import { TaskController } from './task.controller'
-import { Cacheable } from 'cacheable'
-import KeyvRedis from '@keyv/redis'
+// import { Cacheable } from 'cacheable'
+// import KeyvRedis from '@keyv/redis'
 import { CacheService } from './cache.service'
 
 @Module({
@@ -10,15 +10,15 @@ import { CacheService } from './cache.service'
   providers: [
     TaskService,
     CacheService,
-    {
-      provide: 'CACHE_INSTANCE',
-      useFactory: () => {
-        const secondary = new KeyvRedis('redis://3.83.146.231:6379')
+    // {
+    //   provide: 'CACHE_INSTANCE',
+    //   useFactory: () => {
+    //     const secondary = new KeyvRedis('redis://3.83.146.231:6379')
 
-        // const secondary = new KeyvRedis('redis://localhost:6379')
-        return new Cacheable({ secondary, ttl: '4h' })
-      },
-    },
+    //     // const secondary = new KeyvRedis('redis://localhost:6379')
+    //     return new Cacheable({ secondary, ttl: '4h' })
+    //   },
+    // },
   ],
   exports: ['CACHE_INSTANCE'],
 })
